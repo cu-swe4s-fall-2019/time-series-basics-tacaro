8fdd4ffad0f26043fb2bfe3eb855c98dcd50d068
tacaro <tristan.caro@colorado.edu>
2019-10-12 20:21:30 -0600

    first try csv import

--- data_import.py
+++ data_import.py
@@ -11,16 +11,27 @@ class ImportData:
         self._time = []
         self._value = []
 
-        # open file, create a reader from csv.DictReader, and read input times and values
+        # open file, create a reader from csv.DictReader, and read input times
+        # and values
+
+    with open(data_csv, "r") as fhandle:
+        reader = csv.DictReader(fhandle)
+        for row in reader:
+            self._value.append(row['value'])
+            fhandle.close()
 
     def linear_search_value(self, key_time):
         # return list of value(s) associated with key_time
         # if none, return -1 and error message
 
-    def binary_search_value(self,key_time):
+        pass
+
+    def binary_search_value(self, key_time):
         # optional extra credit
         # return list of value(s) associated with key_time
         # if none, return -1 and error message
+        pass
+
 
 def roundTimeArray(obj, res):
     # Inputs: obj (ImportData Object) and res (rounding resoultion)
@@ -33,41 +44,43 @@ def roundTimeArray(obj, res):
     # return: iterable zip object of the two lists
     # note: you can create additional variables to help with this task
     # which are not returned
+    pass
 
 
 def printArray(data_list, annotation_list, base_name, key_file):
     # combine and print on the key_file
+    pass
+
 
 if __name__ == '__main__':
 
-    #adding arguments
-    parser = argparse.ArgumentParser(description= 'A class to import, combine, and print data from a folder.',
-    prog= 'dataImport')
+    # adding arguments
+    parser = argparse.ArgumentParser(description='A class to import, combine,\
+    and print data from a folder.',
+                                     prog='dataImport')
 
-    parser.add_argument('folder_name', type = str, help = 'Name of the folder')
+    parser.add_argument('folder_name', type=str, help='Name of the folder')
 
-    parser.add_argument('output_file', type=str, help = 'Name of Output file')
+    parser.add_argument('output_file', type=str, help='Name of Output file')
 
-    parser.add_argument('sort_key', type = str, help = 'File to sort on')
+    parser.add_argument('sort_key', type=str, help='File to sort on')
 
-    parser.add_argument('--number_of_files', type = int,
-    help = "Number of Files", required = False)
+    parser.add_argument('--number_of_files', type=int,
+                        help="Number of Files", required=False)
 
     args = parser.parse_args()
 
+    # pull all the folders in the file
+    files_lst =  # list the folders
 
-    #pull all the folders in the file
-    files_lst = # list the folders
-
-
-    #import all the files into a list of ImportData objects (in a loop!)
+    # import all the files into a list of ImportData objects (in a loop!)
     data_lst = []
 
-    #create two new lists of zip objects
+    # create two new lists of zip objects
     # do this in a loop, where you loop through the data_lst
-    data_5 = [] # a list with time rounded to 5min
-    data_15 = [] # a list with time rounded to 15min
+    data_5 = []  # a list with time rounded to 5min
+    data_15 = []  # a list with time rounded to 15min
 
-    #print to a csv file
-    printLargeArray(data_5,files_lst,args.output_file+'_5',args.sort_key)
-    printLargeArray(data_15, files_lst,args.output_file+'_15',args.sort_key)
+    # print to a csv file
+    printLargeArray(data_5, files_lst, args.output_file+'_5', args.sort_key)
+    printLargeArray(data_15, files_lst, args.output_file+'_15', args.sort_key)
