1969d347ea77c028d6b59c96ec4b5f6588c42c4b
tacaro <tristan.caro@colorado.edu>
2019-10-13 17:16:10 -0600

    implemented linear search

--- data_import.py
+++ data_import.py
@@ -93,13 +93,75 @@ def roundTimeArray(obj, res):
     # return: iterable zip object of the two lists
     # note: you can create additional variables to help with this task
     # which are not returned
+    time_lst = []
+    vals = []
+    num_times = len(obj._time)
+    type = obj._duphandle
+    for i in range(num_times):
+        time = obj._time[i]
+        bad = datetime.timedelta(minutes=time.minute % res,
+                                 seconds=time.second)
+        time -= bad
+        if (bad >= datetime.timedelta(minutes=math.ceil(res/2))):
+            time += datetime.timedelta(minutes=res)
+        obj._time[i] = time
 
-    pass
+    if num_times > 0:
+        time_lst.append(obj._time[0])
+        sch = obj.linear_search_value(obj.time[0])  # search
+        if type == 0:
+            vals.append(sum(sch))  # summed
+        elif type == 1:
+            vals.append(sum(sch)/len(sch))  # averaged
+
+    for i in range(1, num_times):  # check for duplicates
+        if obj._time[i] == obj._time[i - 1]:
+            continue
+        else:
+            time_lst.append(obj._time[i])
+            sch = obj.linear_search_value(obj._time[i])
+            if type == 0:
+                vals.append(sum(sch))  # summed
+            elif type == 1:
+                vals.append(sum(sch)/len(sch))  # averaged
+    output = zip(time_lst, vals)
+    return output
+    # pass
 
 
 def printArray(data_list, annotation_list, base_name, key_file):
     # combine and print on the key_file
-    pass
+    data_a = []
+    data_b = []
+    annot_a = []
+    annot_b = []
+    if key_file not in annotation_list:
+        print("Key_file not found")
+        return -1
+    else:
+        for i in range(len(annotation_list)):
+            if (annotation_list[i] == key_file):
+                annot_a.append(annotation_list[i])
+                data_a.append(data_list[i])
+            else:
+                annot_b.append(annotation_list[i])
+                data_b.append(data_list[i])
+
+    zipper = ['time', key_file] + annot_b
+    with open(base_name+'.csv', mode='w') as output:
+        wtr = csv.writer(output, delimiter=',')  # define a writer
+        wtr.writerow(zipper)
+        for (time, val) in data_a[0]:
+            old = []
+            for data in data_b:
+                start_length = len(old)
+                for (timex, valsx) in data:
+                    if time == timex:
+                        old.append(valsx)
+                if len(old) == start_length:
+                    old.append(0)
+            wtr.writerow([time, val] + old)
+    # pass
 
 
 if __name__ == '__main__':
